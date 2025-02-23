c618135a101c21f48464e531107e6d14bc00b954
Cash Sun <mr.cashsun@gmail.com>
2025-02-23 21:18:58 +0000

    fix cjs

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,12 @@ export default defineConfig({
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "mylib",
       formats: ["es", "cjs", "umd", "iife"],
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format) => {
+        if(format === 'cjs'){
+          return 'index.cjs';
+        }
+        return `index.${format}.js`
+      },
     },
   },
 } satisfies UserConfig);
