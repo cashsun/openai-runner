e5e36b6a5243e1c045731d30d590c5fa72d58bf6
Cash Sun <mr.cashsun@gmail.com>
2025-03-07 18:32:10 +0000

    version 1.0.7. ability to skip function calling, simplified systemPrompt config.

--- src/openai-runner.spec.ts
+++ src/openai-runner.spec.ts
@@ -16,7 +16,7 @@ const createActions: CreateActions = (context: any) => ({
         `Robot walked for ${secs} seconds`,
         context?.weather ? `in a ${context.weather} weather` : ""
       );
-      return {result: 'done'}
+      return { result: "done" };
     },
     name: "walk",
     description: "instruct robot to walk for given seconds",
@@ -55,18 +55,19 @@ const buildPrompt = async (task: string, context: { weather: string }) => `
 This is your task: ${task}.
 
 * Upon finishing the task, respond to user "Nice robot! And the weather is ${context.weather}" as 
-the final message. Don't saying anything else.
+the final message.
 `;
 
 describe("Openai runner", () => {
   it("can perform simple robot commands", { timeout: 60_000 }, async () => {
     const ai = setup(options, createActions, buildPrompt);
 
-    const result = await ai('Ask the robot to walk for 2 minutes and sit for 1 hour.', {
-      weather: "sunny",
-    });
-
-    console.log('result.message :>> ', result);
+    const result = await ai(
+      "Ask the robot to walk for 2 minutes and sit for 1 hour.",
+      {
+        weather: "sunny",
+      }
+    );
 
     /**
      * expected output
@@ -74,16 +75,65 @@ describe("Openai runner", () => {
      * Function:  walk
      * Params:  {"secs":120}
      * Robot walked for 120 seconds in a sunny weather (from console.log)
-     * Call result:  [empty] 
-     * 
+     * Call result:  [empty]
+     *
      * Function:  sit
      * Params:  {"secs":360}
      * Robot sat for [object Object] seconds in a sunny weather (from console.log)
-     * Call result:  [empty] 
-     * 
-     * Step finished. Final message from AI assistant: 
+     * Call result:  [empty]
+     *
+     * Step finished. Final message from AI assistant:
      * Nice robot! The weather is sunny.
-     * 
+     *
+     *
+     *
+     *   $$$ Token Usage
+     *   ┌───────────────────┬────────┐
+     *   │ (index)           │ Values │
+     *   ├───────────────────┼────────┤
+     *   │ prompt_tokens     │ 79     │
+     *   │ completion_tokens │ 157    │
+     *   │ total_tokens      │ 236    │
+     *   └───────────────────┴────────┘
      */
   });
+
+  it(
+    "can operate normally when no actions provided",
+    { timeout: 60_000 },
+    async () => {
+      const ai = setup(
+        { ...options, systemPrompt: "You are a code generator. Only output code block without extra info." },
+        undefined,
+        (task) => task
+      );
+
+      await ai(
+        "Write a dummy js code to instruct a robot to walk for 5 mins.",
+        { weather: "rainy" }
+      );
+    }
+
+     /**
+     * expected output
+     *
+     * 
+       ```javascript
+        function startRobotWalk() {
+          console.log("Starting the robot's walking routine...");
+          
+          let walkingDuration = 300000; // Duration in milliseconds (5 minutes)
+          setTimeout(endWalkingRoutine, walkingDuration);
+        }
+
+        function endWalkingRoutine() {
+          console.log("The robot has finished its 5-minute walk.");
+        }
+
+        startRobotWalk();
+      ```
+     * 
+     */
+
+  );
 });
